f0f967dd32bc41bc4a9f7357725dc1ce836c6f47
PvtSec <akssofts@gmail.com>
2020-01-30 18:16:12 +0530

    Some changes

--- linear_reg.py
+++ linear_reg.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
+from sklearn.metrics import r2_score
 
 data = pd.read_csv("weather_data.csv", delimiter=',')
 
@@ -14,6 +15,7 @@ yxy=np.reshape(yxy, (-1,1))
 model = linear_model.LinearRegression()
 model.fit(xyx,yxy)
 ypy = model.predict(xyx)
+print(r2_score(xyx,ypy))
 plt.scatter(xyx,yxy)
 plt.plot(xyx,ypy, color='red')
 plt.show()
